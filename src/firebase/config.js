76cf9040221090c5b857f2d58d99dd927f9373df
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2025-07-14 15:46:38 +0300

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzUUKgvADh2XtOatPYcWt2umQbZSyiNF4",
+  authDomain: "test-7b62c.firebaseapp.com",
+  projectId: "test-7b62c",
+  storageBucket: "test-7b62c.firebasestorage.app",
+  messagingSenderId: "402993824560",
+  appId: "1:402993824560:web:78f65da4e841a4446b3ef5"
+};
