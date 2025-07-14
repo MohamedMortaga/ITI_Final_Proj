65c5793bfcf828bb909fe5f6f689457a7212a231
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2025-07-14 18:38:26 +0300

    Fire+Product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,22 @@
     <div class="m-4 text-center">
         <h1 class="text-2xl font-medium text-blue-600 md:text-3xl">Home</h1>
     </div>
+    <div class="m-4 text-center" v-for="product in products" :key="product.id">
+        <p class="text-2xl font-medium text-pink-600 md:text-1xl">{{ product.title }}</p>
+
+    </div>
 </template>
 
 <script>
+import CreateProduct from '../composables/CreateProduct';
+import getCollection from '../composables/getCollection';
     export default {
         name: "HomePage",
+        components:{ CreateProduct },
+        setup() {
+            const { documents: products } = getCollection('products');
+            return { products };
+        }
     }
 </script>
 
