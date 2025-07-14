2d176c40bb3b9bfadbb0240d13f5d151ae21f4cf
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2025-07-14 16:34:21 +0300

    comlast

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,6 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDzUUKgvADh2XtOatPYcWt2umQbZSyiNF4",
   authDomain: "test-7b62c.firebaseapp.com",
@@ -6,3 +9,15 @@ const firebaseConfig = {
   messagingSenderId: "402993824560",
   appId: "1:402993824560:web:78f65da4e841a4446b3ef5"
 };
+
+let db;
+try {
+  // Initialize Firebase only if no apps are initialized
+  const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+  // Initialize Firestore
+  db = getFirestore(app);
+} catch (error) {
+  console.error("Error initializing Firebase:", error);
+}
+
+export { db };
