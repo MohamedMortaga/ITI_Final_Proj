e7eda32640c2c824de5ac53ad45511ed25463c42
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2025-07-19 00:29:25 +0300

    nav

--- src/components/pages/Navbar.vue
+++ src/components/pages/Navbar.vue
@@ -1,12 +1,16 @@
 <template>
   <div :class="[isDarkMode ? 'dark' : 'light']">
-    <Disclosure as="nav" class="bg-[var(--Color-Surface-Surface-Primary)] text-[var(--Color-Text-Text-Primary)] mb-0">
+    <Disclosure
+      as="nav"
+      class="bg-[var(--Color-Surface-Surface-Primary)] text-[var(--Color-Text-Text-Primary)] mb-0"
+    >
       <div class="mx-auto container px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
           <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <!-- Mobile menu button -->
             <DisclosureButton
-              class="inline-flex items-center justify-center rounded-md p-2 text-[var(--Color-Text-Text-Primary)] hover:bg-[var(--Color-Surface-Surface-Secondary)] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-[var(--Color-Boarder-Border-Primary)]">
+              class="inline-flex items-center justify-center rounded-md p-2 text-[var(--Color-Text-Text-Primary)] hover:bg-[var(--Color-Surface-Surface-Secondary)] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-[var(--Color-Boarder-Border-Primary)]"
+            >
               <span class="sr-only">Open main menu</span>
               <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
               <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
@@ -14,53 +18,79 @@
           </div>
 
           <!-- Desktop navbar -->
-          <div class="flex items-center justify-between w-full min-[641px]:flex max-[640px]:hidden">
-            <router-link to="/" class="text-xl font-medium hover:text-[var(--Color-Text-Text-Brand)]"
+          <div
+            class="flex items-center justify-between w-full min-[641px]:flex max-[640px]:hidden"
+          >
+            <router-link
+              to="/"
+              class="text-xl font-medium hover:text-[var(--Color-Text-Text-Brand)]"
               :class="{
-                'text-[var(--Color-Text-Text-Brand)]': true
-              }">
+                'text-[var(--Color-Text-Text-Brand)]': true,
+              }"
+            >
               Rento
             </router-link>
 
             <div class="flex-1 flex items-center justify-center">
-              <div class="flex space-x-[64px]">
-                <router-link to="/home" class="text-xl font-medium hover:text-[var(--Color-Text-Text-Brand)]"
+              <div class="flex lg:space-x-[64px] md:space-x-[16px] space-x-[8px]">
+                <router-link
+                  to="/home"
+                  class="lg:text-xl md:text-md text-sm font-medium hover:text-[var(--Color-Text-Text-Brand)]"
                   :class="{
                     'text-[var(--Color-Text-Text-Brand)]': $route.path === '/home',
-                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]': $route.path !== '/home'
-                  }">
+                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]':
+                      $route.path !== '/home',
+                  }"
+                >
                   Home
                 </router-link>
 
-                <router-link to="/browse-tools" class="text-xl font-medium hover:text-[var(--Color-Text-Text-Brand)]"
+                <router-link
+                  to="/browse-tools"
+                  class="lg:text-xl md:text-md text-sm font-medium hover:text-[var(--Color-Text-Text-Brand)]"
                   :class="{
-                    'text-[var(--Color-Text-Text-Brand)]': $route.path === '/browse-tools',
-                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]': $route.path !== '/browse-tools'
-                  }">
+                    'text-[var(--Color-Text-Text-Brand)]':
+                      $route.path === '/browse-tools',
+                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]':
+                      $route.path !== '/browse-tools',
+                  }"
+                >
                   Browse Tools
                 </router-link>
 
-                <router-link to="/addProduct" class="text-xl font-medium hover:text-[var(--Color-Text-Text-Brand)]"
+                <router-link
+                  to="/addProduct"
+                  class="lg:text-xl md:text-md text-sm font-medium hover:text-[var(--Color-Text-Text-Brand)]"
                   :class="{
                     'text-[var(--Color-Text-Text-Brand)]': $route.path === '/addProduct',
-                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]': $route.path !== '/addProduct'
-                  }">
+                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]':
+                      $route.path !== '/addProduct',
+                  }"
+                >
                   Add Your Tool
                 </router-link>
 
-                <router-link to="/about" class="text-xl font-medium hover:text-[var(--Color-Text-Text-Brand)]"
+                <router-link
+                  to="/about"
+                  class="lg:text-xl md:text-md text-sm font-medium hover:text-[var(--Color-Text-Text-Brand)]"
                   :class="{
                     'text-[var(--Color-Text-Text-Brand)]': $route.path === '/about',
-                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]': $route.path !== '/about'
-                  }">
+                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]':
+                      $route.path !== '/about',
+                  }"
+                >
                   About
                 </router-link>
 
-                <router-link to="/contact" class="text-xl font-medium hover:text-[var(--Color-Text-Text-Brand)]"
+                <router-link
+                  to="/contact"
+                  class="lg:text-xl md:text-md text-sm font-medium hover:text-[var(--Color-Text-Text-Brand)]"
                   :class="{
                     'text-[var(--Color-Text-Text-Brand)]': $route.path === '/contact',
-                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]': $route.path !== '/contact'
-                  }">
+                    'text-[var(--Color-Text-Text-Primary)] hover:text-[var(--Color-Text-Text-Brand)]':
+                      $route.path !== '/contact',
+                  }"
+                >
                   Contact
                 </router-link>
               </div>
@@ -68,31 +98,126 @@
 
             <div class="flex items-center space-x-4">
               <div>
-                <input type="checkbox" class="custom-toggle" id="checkbox" v-model="isDarkMode" />
+                <input
+                  type="checkbox"
+                  class="custom-toggle"
+                  id="checkbox"
+                  v-model="isDarkMode"
+                />
                 <label for="checkbox" class="custom-toggle-label">
                   <span class="toggle-icon"></span>
                 </label>
               </div>
 
               <!-- Login -->
-              <router-link v-if="!isAuthenticated" to="/login"
-                class="px-4 py-2 rounded-md text-xl font-medium bg-[var(--Color-Surface-Surface-Brand)] hover:bg-[var(--Colors-Primary-700)] active:bg-[var(--Colors-Primary-700)] focus:bg-[var(--Colors-Primary-700)] text-[var(--Color-Text-Text-Invert)]">
+              <router-link
+                v-if="!isAuthenticated"
+                to="/login"
+                class="px-4 py-2 rounded-md text-xl font-medium bg-[var(--Color-Surface-Surface-Brand)] hover:bg-[var(--Colors-Primary-700)] active:bg-[var(--Colors-Primary-700)] focus:bg-[var(--Colors-Primary-700)] text-[var(--Color-Text-Text-Invert)]"
+              >
                 Login
               </router-link>
 
               <!-- Logout -->
-              <button v-else @click="handleLogout"
-                class="px-4 py-2 rounded-md text-xl font-medium bg-[var(--Color-Surface-Surface-Brand)] hover:bg-[var(--Colors-Primary-700)] active:bg-[var(--Colors-Primary-700)] focus:bg-[var(--Colors-Primary-700)] text-[var(--Color-Text-Text-Invert)]">
+              <button
+                v-else
+                @click="handleLogout"
+                class="px-4 py-2 rounded-md text-xl font-medium bg-[var(--Color-Surface-Surface-Brand)] hover:bg-[var(--Colors-Primary-700)] active:bg-[var(--Colors-Primary-700)] focus:bg-[var(--Colors-Primary-700)] text-[var(--Color-Text-Text-Invert)]"
+              >
                 Logout
               </button>
             </div>
           </div>
         </div>
       </div>
+      <DisclosurePanel class="sm:hidden">
+        <div class="space-y-1 px-2 pt-2 pb-3 flex flex-col bg-white dark:bg-gray-900">
+          <router-link
+            to="/"
+            class="text-primary-500 hover:bg-gray-200 hover:text-primary-700 rounded-md px-3 py-2 text-xl font-medium"
+            :class="{
+              'text-primary-900': $route.path === '/',
+              'dark:text-primary-400 dark:hover:bg-gray-700': isDarkMode,
+            }"
+          >
+            Rento
+          </router-link>
+          <router-link
+            to="/home"
+            class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-xl font-medium"
+            :class="{
+              'text-gray-900': $route.path === '/home',
+              'dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white': isDarkMode,
+            }"
+          >
+            Home
+          </router-link>
+          <router-link
+            to="/browse-tools"
+            class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-xl font-medium"
+            :class="{
+              'text-gray-900': $route.path === '/browse-tools',
+              'dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white': isDarkMode,
+            }"
+          >
+            Browse Tools
+          </router-link>
+          <router-link
+            to="/addProduct"
+            class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-xl font-medium"
+            :class="{
+              'text-gray-900': $route.path === '/addProduct',
+              'dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white': isDarkMode,
+            }"
+          >
+            Add Your Tool
+          </router-link>
+          <router-link
+            to="/about"
+            class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-xl font-medium"
+            :class="{
+              'text-gray-900': $route.path === '/about',
+              'dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white': isDarkMode,
+            }"
+          >
+            About
+          </router-link>
+          <router-link
+            to="/contact"
+            class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-xl font-medium"
+            :class="{
+              'text-gray-900': $route.path === '/contact',
+              'dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white': isDarkMode,
+            }"
+          >
+            Contact
+          </router-link>
+          <router-link
+            to="/login"
+            class="bg-success-500 text-white hover:bg-success-600 rounded-md px-3 py-2 text-xl font-medium"
+            :class="{
+              'bg-success-700': $route.path === '/login',
+              'dark:bg-success-600': isDarkMode,
+            }"
+          >
+            Logout
+          </router-link>
+          <div class="mt-2">
+            <input
+              type="checkbox"
+              class="custom-toggle"
+              id="mobile-checkbox"
+              v-model="isDarkMode"
+            />
+            <label for="mobile-checkbox" class="custom-toggle-label">
+              <span class="toggle-icon"></span>
+            </label>
+          </div>
+        </div>
+      </DisclosurePanel>
     </Disclosure>
   </div>
 </template>
-
 
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
@@ -189,7 +314,8 @@ watch(isDarkMode, (newValue) => {
 }
 
 .custom-toggle-label .toggle-icon {
-  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="teal" width="24px" height="24px"><circle cx="12" cy="12" r="6" stroke="teal" stroke-width="2" fill="none"/><path d="M12 2a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16z"/></svg>') no-repeat center;
+  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="teal" width="24px" height="24px"><circle cx="12" cy="12" r="6" stroke="teal" stroke-width="2" fill="none"/><path d="M12 2a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16z"/></svg>')
+    no-repeat center;
   width: 26px;
   height: 26px;
   position: absolute;
@@ -199,12 +325,13 @@ watch(isDarkMode, (newValue) => {
   transition: transform 0.3s ease, background 0.3s ease;
 }
 
-.custom-toggle:checked+.custom-toggle-label {
+.custom-toggle:checked + .custom-toggle-label {
   background-color: #a3e4d7;
 }
 
-.custom-toggle:checked+.custom-toggle-label .toggle-icon {
+.custom-toggle:checked + .custom-toggle-label .toggle-icon {
   transform: translateX(26px);
-  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="teal" width="24px" height="24px"><circle cx="12" cy="12" r="6" stroke="teal" stroke-width="2" fill="none"/><path d="M12 2a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16z"/></svg>') no-repeat center;
+  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="teal" width="24px" height="24px"><circle cx="12" cy="12" r="6" stroke="teal" stroke-width="2" fill="none"/><path d="M12 2a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16z"/></svg>')
+    no-repeat center;
 }
 </style>
