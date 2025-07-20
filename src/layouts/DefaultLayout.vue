23dec25c0660a263f2743ca0389c3b8181cdf8b4
Sama Ibrahim <samaebrahim322@gmail.com>
2025-07-20 04:58:08 +0300

    edit in nvabar

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="min-h-screen bg-white">
-    <Navbar />
-    <main >
+    <Navbar v-if="!route.meta.hideNavbar" />
+    
+    <main>
       <slot />
     </main>
   </div>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router'
 import Navbar from '@/components/pages/Navbar.vue'
+
+const route = useRoute()
 </script>
